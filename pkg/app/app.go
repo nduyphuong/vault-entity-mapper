994d4079a8a65e027119dfc6a68e15e957705f3a
phuongnd96 <nguyenduyphuong_t59@hus.edu.vn>
2023-08-08 13:53:35 +0700

    handle update and some edge cases

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"fmt"
 
 	client "github.com/hashicorp/vault-client-go"
 	"github.com/hashicorp/vault-client-go/schema"
@@ -23,43 +24,38 @@ func Run(conf config.Config) error {
 	}
 	var eId []string
 	for _, v := range conf.Entities {
-		if v.Deactived {
-			r, _ := c.Identity.EntityLookUp(ctx, schema.EntityLookUpRequest{
-				Name: v.Name,
-			})
-			if r != nil {
-				if r.Data != nil {
-					var d EntityLookUpResponse
-					config := &mapstructure.DecoderConfig{
-						ErrorUnused: true,
-						Result:      &d,
-					}
-					decoder, err := mapstructure.NewDecoder(config)
-					if err != nil {
-						log.Errorf("decode entity lookup response: %v", err)
-					}
-					decoder.Decode(r.Data)
-					eId = append(eId, d.Id)
-				}
-			}
-			continue
-		}
-		_, err := c.Identity.EntityLookUp(ctx, schema.EntityLookUpRequest{
+		var d EntityLookUpResponse
+		r, _ := c.Identity.EntityLookUp(ctx, schema.EntityLookUpRequest{
 			Name: v.Name,
 		})
-		if err != nil {
-			_, err := c.Identity.EntityCreate(ctx, schema.EntityCreateRequest{
-				Disabled: v.Disabled,
-				Id:       v.Id,
-				Metadata: v.Metadata,
-				Name:     v.Name,
-				Policies: v.Policies,
-			})
-			if err != nil {
-				log.Errorf("create entity %s: %v", v.Name, err)
+		if r != nil {
+			if r.Data != nil {
+				config := &mapstructure.DecoderConfig{
+					ErrorUnused: true,
+					Result:      &d,
+				}
+				decoder, err := mapstructure.NewDecoder(config)
+				if err != nil {
+					log.Errorf("decode entity lookup response: %v", err)
+				}
+				decoder.Decode(r.Data)
 			}
-			log.Infof("created entity %s", v.Name)
 		}
+		if v.Deactived {
+			eId = append(eId, d.Id)
+			continue
+		}
+		_, err := c.Identity.EntityCreate(ctx, schema.EntityCreateRequest{
+			Disabled: v.Disabled,
+			Metadata: v.Metadata,
+			Id:       d.Id,
+			Name:     v.Name,
+			Policies: v.Policies,
+		})
+		if err != nil {
+			log.Errorf("create entity %s: %v", v.Name, err)
+		}
+		log.Infof("entity %s was created", v.Name)
 	}
 	// delete if entity disabled in config file
 	// safe delete in config file after disabled
@@ -74,39 +70,63 @@ func Run(conf config.Config) error {
 			log.Errorf("delete entity: %v", err)
 		}
 	}
-	toDel := map[string]string{}
 	for _, v := range conf.EntityAliases {
 		if v.Deactived {
-			toDel[v.Name] = ""
 			continue
 		}
 		_, err := c.Identity.AliasCreate(ctx, schema.AliasCreateRequest{
 			Name:          v.Name,
 			CanonicalId:   v.CanonicalId,
 			MountAccessor: v.MountAccessor,
-			Id:            v.Id,
 		})
 		if err != nil {
 			log.Errorf("create entity alias %s: %v", v.Name, err)
 		}
-		log.Infof("created entity alias %s", v.Name)
+		log.Infof("entity alias %s was created", v.Name)
 	}
-	r, err := c.Identity.EntityListAliasesById(ctx)
-	if err != nil {
-		log.Errorf("list entity aliases: %v", err)
-		return err
+	eAtoI := map[string]map[string]interface{}{}
+	r, _ := c.Identity.EntityListAliasesById(ctx)
+	if r == nil {
+		return nil
 	}
-
-	for k, tmp := range r.Data["key_info"].(map[string]interface{}) {
-		v := tmp.(map[string]interface{})["name"].(string)
-		if _, ok := toDel[v]; ok {
-			toDel[v] = k
+	if r.Data != nil {
+		for k, tmp := range r.Data["key_info"].(map[string]interface{}) {
+			v := tmp.(map[string]interface{})["name"].(string)
+			eAtoI[v] = map[string]interface{}{
+				"Id":        k,
+				"Deactived": false,
+			}
 		}
 	}
-	for k, v := range toDel {
-		log.Warnf("entity aliases %s id: %s disabled in config, deleting", k, v)
-		if _, err := c.Identity.EntityDeleteAliasById(ctx, v); err != nil {
-			log.Errorf("delete %s id: %s", k, v)
+	fmt.Printf("eAtoI: %v\n", eAtoI)
+	for _, v := range conf.EntityAliases {
+		if v.Deactived {
+			if _, ok := eAtoI[v.Name]; ok {
+				eAtoI[v.Name]["Deactived"] = true
+			}
+			continue
+		}
+		if _, ok := eAtoI[v.Name]; !ok {
+			_, err := c.Identity.AliasCreate(ctx, schema.AliasCreateRequest{
+				Name:          v.Name,
+				CanonicalId:   v.CanonicalId,
+				MountAccessor: v.MountAccessor,
+				Id:            eAtoI[v.Name]["Id"].(string),
+			})
+			if err != nil {
+				log.Errorf("update entity alias %s: %v", v.Name, err)
+			}
+		}
+	}
+	fmt.Printf("eAtoI: %v\n", eAtoI)
+	for k, v := range eAtoI {
+		if v["Deactived"] == true {
+			if _, err := c.Identity.AliasReadById(ctx, v["Id"].(string)); err == nil {
+				log.Warnf("entity aliases %s  disabled in config, deleting", k)
+				if _, err := c.Identity.EntityDeleteAliasById(ctx, v["Id"].(string)); err != nil {
+					log.Errorf("delete %s", k)
+				}
+			}
 		}
 	}
 	return nil
